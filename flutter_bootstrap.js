5713ea674699510714cc09ecaf401d4714be3b1b
SUNIL MULGUND <mulgundsunil1918@gmail.com>
2026-06-20 13:45:00 +0530

    fix: educational disclaimer, citations, iOS gate on dose calculators (Apple 1.4.1 & 1.4.2)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2106823798" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2895573871" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
