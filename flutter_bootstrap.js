30a947175616b46436b6c831ca25de1baeb41151
SUNIL MULGUND <mulgundsunil1918@gmail.com>
2026-07-10 00:30:30 +0530

    Deploy: admin Send Notification screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3220774564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2579123403" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
