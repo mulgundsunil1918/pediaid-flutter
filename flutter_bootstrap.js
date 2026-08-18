7cbb912705c4a41f0af3708156bbe8eeccc58e3a
mulgundsunil1918 <mulgundsunil@gmail.com>
2026-08-18 16:24:57 +0530

    web: WHO results — loader-gate + preload all data + render everything before open (no per-tab flash)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "391254103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2197155761" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
